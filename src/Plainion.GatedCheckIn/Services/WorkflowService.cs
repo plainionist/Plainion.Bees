6e01709b5fc4191652a0a84cd7b4ae2cbb91a7b9
ronin4net <stanley751@gmx.net>
2015-09-15 11:47:21 +0200

    GatedCheckIn tool started

---
+++ src/Plainion.GatedCheckIn/Services/WorkflowService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Plainion.GatedCheckIn.Services
+{
+    [Export]
+    class WorkflowService
+    {
+        public Task<bool> ExecuteAsync(string Solution, bool RunTests, bool CheckIn, IProgress<string> progress)
+        {
+            return Task<bool>.Run(() =>
+                {
+                    Thread.Sleep(10 * 1000);
+                    return false;
+                });
+        }
+    }
+}
---
+++ src/Plainion.GatedCheckIn/ShellViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+using Plainion.GatedCheckIn.Services;
+
+namespace Plainion.GatedCheckIn
+{
+    [Export]
+    class ShellViewModel : BindableBase
+    {
+        private WorkflowService myWorkflowService;
+        private string mySolution;
+        private bool myRunTests;
+        private bool myCheckIn;
+        private bool myIsBusy;
+        private bool? mySucceeded;
+
+        [ImportingConstructor]
+        public ShellViewModel(WorkflowService workflowService)
+        {
+            myWorkflowService = workflowService;
+
+            GoCommand = new DelegateCommand(OnGo, CanGo);
+            Messages = new ObservableCollection<string>();
+
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                Solution = args[1];
+            }
+        }
+
+        public string Solution
+        {
+            get { return mySolution; }
+            set { SetProperty(ref mySolution, value); }
+        }
+
+        public bool RunTests
+        {
+            get { return myRunTests; }
+            set { SetProperty(ref myRunTests, value); }
+        }
+
+        public bool CheckIn
+        {
+            get { return myCheckIn; }
+            set { SetProperty(ref myCheckIn, value); }
+        }
+
+        public DelegateCommand GoCommand { get; private set; }
+
+        private bool CanGo() { return !myIsBusy; }
+
+        private void OnGo()
+        {
+            Messages.Clear();
+            myIsBusy = true;
+            GoCommand.RaiseCanExecuteChanged();
+
+            var progress = new Progress<string>(p => Messages.Add(p));
+
+            myWorkflowService.ExecuteAsync(Solution, RunTests, CheckIn, progress)
+                .ContinueWith(t =>
+                    {
+                        Succeeded = t.Result;
+
+                        myIsBusy = false;
+                        GoCommand.RaiseCanExecuteChanged();
+                    }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        public ObservableCollection<string> Messages { get; private set; }
+
+        public bool? Succeeded
+        {
+            get { return mySucceeded; }
+            set { SetProperty(ref mySucceeded, value); }
+        }
+    }
+}
