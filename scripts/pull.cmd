43c67ce4f23166afddbe5e3fd663f5b84f330f26
ronin4net <stanley751@gmx.net>
2016-08-21 16:51:25 +0200

    pull script updated

--- scripts/pull.cmd
+++ scripts/pull.cmd
@@ -1,3 +1,6 @@
+@echo off
+setlocal
+
 rem Pulls latest from all GIT repositories
 rem 
 rem Usage
@@ -6,9 +9,6 @@ rem put this script into the folder where all your git repositories are located 
 rem the .git folder is located) and just run it. 
 rem The script will assume all subfolders are git repositories and try to run "git pull"
 rem
-
-@echo off
-setlocal
 
 for /f "delims=" %%i in ('dir /ad /b') do (
   echo --- %%i ---
