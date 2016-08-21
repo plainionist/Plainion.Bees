b50b007d8f824800bf779adb6c28efbab672dd37
ronin4net <stanley751@gmx.net>
2016-08-21 16:55:20 +0200

    pull script fixed

--- scripts/pull.cmd
+++ scripts/pull.cmd
@@ -7,14 +7,16 @@ rem Usage
 rem =====
 rem put this script into the folder where all your git repositories are located (so outside the folder where
 rem the .git folder is located) and just run it. 
-rem The script will assume all subfolders are git repositories and try to run "git pull"
+rem The script will assume all subfolders containing a ".git" folder are git repositories and try to run "git pull"
 rem
 
 for /f "delims=" %%i in ('dir /ad /b') do (
-  echo --- %%i ---
-  cd %%i
-  git pull
-  cd ..
+  if exist %%i\.git (
+    echo --- %%i ---
+    cd %%i
+    git pull
+    cd ..
+  )
 )
 
 pause
