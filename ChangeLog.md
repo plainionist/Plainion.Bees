8ffeffb2ff0a5ce2055352156f68d6cc30a8366a
plainionist <pinguin751@gmx.net>
2017-02-17 11:15:25 +0100

    targets.fsx fixed

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,7 @@
+## 1.13.0 - 2017-02-17
+
+- updated to Plainion.AppFW.Shell 1.3.2
+
 ## 1.12.0 - 2016-08-13
 
 - TestIt: no shadow copies
--- src/Plainion.Scripts/Properties/AssemblyInfo.cs
+++ src/Plainion.Scripts/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Reflection;
 [assembly: AssemblyProductAttribute("Plainion.Bees")]
 [assembly: AssemblyDescriptionAttribute("Plainion.Bees")]
 [assembly: AssemblyCopyrightAttribute("Copyright @ 2017")]
-[assembly: AssemblyVersionAttribute("1.12.0")]
-[assembly: AssemblyFileVersionAttribute("1.12.0")]
+[assembly: AssemblyVersionAttribute("1.13.0")]
+[assembly: AssemblyFileVersionAttribute("1.13.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Plainion.Scripts";
         internal const System.String AssemblyProduct = "Plainion.Bees";
         internal const System.String AssemblyDescription = "Plainion.Bees";
         internal const System.String AssemblyCopyright = "Copyright @ 2017";
-        internal const System.String AssemblyVersion = "1.12.0";
-        internal const System.String AssemblyFileVersion = "1.12.0";
+        internal const System.String AssemblyVersion = "1.13.0";
+        internal const System.String AssemblyFileVersion = "1.13.0";
     }
 }
