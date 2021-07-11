fb05a892f7c4c2452de203b975e3dd83e65a4002
plainionnist <stanley751@gmx.net>
2021-07-11 08:10:15 +0200

    Code cleanup

--- src/Plainion.WhiteRabbit/Presentation/Reports/DayReport.cs
+++ src/Plainion.WhiteRabbit/Presentation/Reports/DayReport.cs
@@ -5,15 +5,19 @@ namespace Plainion.WhiteRabbit.Presentation.Reports
 {
     public partial class DayReport : ReportBase
     {
+        public DateTime Day { get; set; }
+        public Dictionary<string, TimeSpan> Data { get; set; }
+        public bool IsComplete { get; set; }
+
         public virtual string TransformText()
         {
-            this.Write("\r\n\r\n<html>\r\n<head>\r\n    <title>WhiteRabbit</title>\r\n</head>\r\n<body>\r\n    <center>" +
+            Write("\r\n\r\n<html>\r\n<head>\r\n    <title>WhiteRabbit</title>\r\n</head>\r\n<body>\r\n    <center>" +
                     "\r\n        <h2>\r\n            WhiteRabbit</h2>\r\n    </center>\r\n    <center>\r\n     " +
                     "   <h4>\r\n            ");
 
-            this.Write(this.ToStringHelper.ToStringWithCulture(Day.Date.ToShortDateString()));
+            Write(ToStringWithCulture(Day.Date.ToShortDateString()));
 
-            this.Write("</h4>\r\n    </center>\r\n    <table border=\"0\" cellpadding=\"4\" cellspacing=\"0\">\r\n   " +
+            Write("</h4>\r\n    </center>\r\n    <table border=\"0\" cellpadding=\"4\" cellspacing=\"0\">\r\n   " +
                     "     <tr>\r\n            <th>\r\n                Comment\r\n            </th>\r\n " +
                     "           <th style=\"width:75px;\">Time</th>\r\n        </tr>\r\n        ");
 
@@ -28,44 +32,40 @@ namespace Plainion.WhiteRabbit.Presentation.Reports
 
                 sum += Data[comment];
 
-                this.Write("        <tr>\r\n            <td>");
-                this.Write(this.ToStringHelper.ToStringWithCulture(comment));
-                this.Write("</td>\r\n            <td align=\"right\">");
-                this.Write(this.ToStringHelper.ToStringWithCulture(Data[comment].ToReportString()));
-                this.Write("</td>\r\n        </tr>\r\n        ");
+                Write("        <tr>\r\n            <td>");
+                Write(ToStringWithCulture(comment));
+                Write("</td>\r\n            <td align=\"right\">");
+                Write(ToStringWithCulture(Data[comment].ToReportString()));
+                Write("</td>\r\n        </tr>\r\n        ");
             }
 
-            this.Write("        \r\n        ");
+            Write("        \r\n        ");
 
             if (Data["unknown"] != TimeSpan.Zero)
             {
                 sum += Data["unknown"];
 
-                this.Write("        <tr>\r\n            <td>unknown</td>\r\n            <td align=\"right\">");
-                this.Write(this.ToStringHelper.ToStringWithCulture(Data["unknown"].ToReportString()));
-                this.Write("</td>\r\n        </tr>\r\n        ");
+                Write("        <tr>\r\n            <td>unknown</td>\r\n            <td align=\"right\">");
+                Write(ToStringWithCulture(Data["unknown"].ToReportString()));
+                Write("</td>\r\n        </tr>\r\n        ");
             }
 
-            this.Write("        <tr>\r\n            <td style=\"border-top:solid 2px #060\">\r\n               " +
+            Write("        <tr>\r\n            <td style=\"border-top:solid 2px #060\">\r\n               " +
                     " <b>Sum</b>\r\n            </td>\r\n            <td  align=\"right\" style=\"border-top" +
                     ":solid 2px #060\">\r\n                ");
 
-            this.Write(this.ToStringHelper.ToStringWithCulture(sum.ToReportString()));
+            Write(ToStringWithCulture(sum.ToReportString()));
 
-            this.Write("\r\n            </td>\r\n        </tr>\r\n    </table>\r\n    \r\n    <br />\r\n    \r\n    ");
+            Write("\r\n            </td>\r\n        </tr>\r\n    </table>\r\n    \r\n    <br />\r\n    \r\n    ");
 
             if (!IsComplete)
             {
-                this.Write("    <font color=\"red\">The report is not complete because the data of the day is n" +
+                Write("    <font color=\"red\">The report is not complete because the data of the day is n" +
                         "ot complete.</font>\r\n");
             }
 
-            this.Write("</body>\r\n</html>\r\n\r\n");
-            return this.GenerationEnvironment.ToString();
+            Write("</body>\r\n</html>\r\n\r\n");
+            return GenerationEnvironment.ToString();
         }
-
-        public DateTime Day { get; set; }
-        public Dictionary<string, TimeSpan> Data { get; set; }
-        public bool IsComplete { get; set; }
     }
 }
--- src/Plainion.WhiteRabbit/Presentation/Reports/ReportBase.cs
+++ src/Plainion.WhiteRabbit/Presentation/Reports/ReportBase.cs
@@ -1,31 +1,31 @@
-﻿namespace Plainion.WhiteRabbit.Presentation.Reports
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Plainion.WhiteRabbit.Presentation.Reports
 {
     public class ReportBase
     {
-        private global::System.Text.StringBuilder generationEnvironmentField;
-        private bool endsWithNewline;
+        private StringBuilder myGenerationEnvironmentField;
+        private readonly IFormatProvider myFormatProvider = CultureInfo.InvariantCulture;
+        private bool myEndsWithNewline;
 
-        /// <summary>
-        /// The string builder that generation-time code is using to assemble generated output
-        /// </summary>
-        protected System.Text.StringBuilder GenerationEnvironment
+        protected StringBuilder GenerationEnvironment
         {
             get
             {
-                if ((this.generationEnvironmentField == null))
+                if ((myGenerationEnvironmentField == null))
                 {
-                    this.generationEnvironmentField = new global::System.Text.StringBuilder();
+                    myGenerationEnvironmentField = new StringBuilder();
                 }
-                return this.generationEnvironmentField;
+                return myGenerationEnvironmentField;
             }
             set
             {
-                this.generationEnvironmentField = value;
+                myGenerationEnvironmentField = value;
             }
         }
-        /// <summary>
-        /// Write text directly into the generated output
-        /// </summary>
+
         public void Write(string textToAppend)
         {
             if (string.IsNullOrEmpty(textToAppend))
@@ -34,84 +34,63 @@
             }
             // If we're starting off, or if the previous text ended with a newline,
             // we have to append the current indent first.
-            if (((this.GenerationEnvironment.Length == 0)
-                        || this.endsWithNewline))
+            if (((GenerationEnvironment.Length == 0) || myEndsWithNewline))
             {
-                this.GenerationEnvironment.Append("");
-                this.endsWithNewline = false;
+                GenerationEnvironment.Append("");
+                myEndsWithNewline = false;
             }
             // Check if the current text ends with a newline
             if (textToAppend.EndsWith(global::System.Environment.NewLine, global::System.StringComparison.CurrentCulture))
             {
-                this.endsWithNewline = true;
+                myEndsWithNewline = true;
             }
             // This is an optimization. If the current indent is "", then we don't have to do any
             // of the more complex stuff further down.
             if (("".Length == 0))
             {
-                this.GenerationEnvironment.Append(textToAppend);
+                GenerationEnvironment.Append(textToAppend);
                 return;
             }
             // Everywhere there is a newline in the text, add an indent after it
             textToAppend = textToAppend.Replace(global::System.Environment.NewLine, (global::System.Environment.NewLine + ""));
             // If the text ends with a newline, then we should strip off the indent added at the very end
             // because the appropriate indent will be added when the next time Write() is called
-            if (this.endsWithNewline)
+            if (myEndsWithNewline)
             {
-                this.GenerationEnvironment.Append(textToAppend, 0, (textToAppend.Length - "".Length));
+                GenerationEnvironment.Append(textToAppend, 0, (textToAppend.Length - "".Length));
             }
             else
             {
-                this.GenerationEnvironment.Append(textToAppend);
+                GenerationEnvironment.Append(textToAppend);
             }
         }
-        /// <summary>
-        /// Write text directly into the generated output
-        /// </summary>
+
         public void WriteLine(string textToAppend)
         {
-            this.Write(textToAppend);
-            this.GenerationEnvironment.AppendLine();
-            this.endsWithNewline = true;
+            Write(textToAppend);
+            GenerationEnvironment.AppendLine();
+            myEndsWithNewline = true;
         }
+
         /// <summary>
-        /// Utility class to produce culture-oriented representation of an object as a string.
+        /// This is called from the compile/run appdomain to convert objects within an expression block to a string
         /// </summary>
-        public class ToStringInstanceHelper
+        public string ToStringWithCulture(object objectToConvert)
         {
-            private System.IFormatProvider formatProviderField = global::System.Globalization.CultureInfo.InvariantCulture;
-            /// <summary>
-            /// This is called from the compile/run appdomain to convert objects within an expression block to a string
-            /// </summary>
-            public string ToStringWithCulture(object objectToConvert)
+            if ((objectToConvert == null))
             {
-                if ((objectToConvert == null))
-                {
-                    throw new global::System.ArgumentNullException("objectToConvert");
-                }
-                System.Type t = objectToConvert.GetType();
-                System.Reflection.MethodInfo method = t.GetMethod("ToString", new System.Type[] {
-                            typeof(System.IFormatProvider)});
-                if ((method == null))
-                {
-                    return objectToConvert.ToString();
-                }
-                else
-                {
-                    return ((string)(method.Invoke(objectToConvert, new object[] {
-                                this.formatProviderField })));
-                }
+                throw new global::System.ArgumentNullException("objectToConvert");
             }
-        }
-        private ToStringInstanceHelper toStringHelperField = new ToStringInstanceHelper();
-        /// <summary>
-        /// Helper to produce culture-oriented representation of an object as a string
-        /// </summary>
-        public ToStringInstanceHelper ToStringHelper
-        {
-            get
+
+            var t = objectToConvert.GetType();
+            var method = t.GetMethod("ToString", new Type[] { typeof(System.IFormatProvider) });
+            if ((method == null))
             {
-                return this.toStringHelperField;
+                return objectToConvert.ToString();
+            }
+            else
+            {
+                return ((string)(method.Invoke(objectToConvert, new object[] { myFormatProvider })));
             }
         }
     }
